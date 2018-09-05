49eb1ca5c4a5ec8088bad321ba11b882bd8fd7cd
Henry2110 <henryvalladores@gmail.com>
2018-09-04 20:11:48 -0500

    create css

--- client/src/index.js
+++ client/src/index.js
@@ -1,8 +1,17 @@
-import React from "react";
+import React, {Component}from "react";
 import ReactDOM from "react-dom";
 
 const Index = () => (
-    <div> Hello React</div>;
+  <center>
+
+  <h1>
+<a href="https://imgur.com/rTcOFGo"><img src="https://i.imgur.com/rTcOFGo.jpg" title="source: imgur.com" /></a>
+    <div>Segunda Pata</div>
+
+    </h1>
+    <h2>Vende y Compra accesorios, para mascotas.</h2>
+    </center>
     )
 
 ReactDOM.render(<Index />, document.getElementById("index"));
+export default Index;
