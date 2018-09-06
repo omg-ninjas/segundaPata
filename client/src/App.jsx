7a7672d8e6730bdc776871d8c8ba498b9c65d663
jazzcelaya <jazzcelaya@gmail.com>
2018-09-06 16:38:29 -0500

    navigation bar rendering and working

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,28 +1,29 @@
 import React, {Component}from "react";
 import ReactDOM from "react-dom";
-import MenuItem from "./components/search/menuItem.js";
 import {BrowserRouter, Route, Switch} from "react-router-dom";
+
+import MenuItem from "./components/search/menuItem.js";
 import ItemPage from "./components/item-page/ItemPage.jsx";
+import Error from "./components/item-page/error.js";
+import Navigation from "./components/item-page/navigation.js";
+import Home from "./home.jsx";
 
 class App extends Component {
-render(){
-  return(
-    <div>
-  <center>
-
-    <h1>
-  <a href="https://imgur.com/rTcOFGo"><img src="https://i.imgur.com/rTcOFGo.jpg" title="source: imgur.com" /></a>
-      <div>Segunda Pata</div>
-    </h1>
-      <h2>Vende y Compra Accesorios, para Mascotas.</h2>
-      <MenuItem/>
-      <ItemPage />
-      </center>
-      </div>
-    )
+  render() {
+    return (
+      <BrowserRouter>
+        <div>
+          <Navigation />
+          <Switch>
+            <Route path="/" component={Home} exact />
+            <Route path="/item" component={ItemPage} />
+            <Route component={Error} />
+          </Switch>
+        </div>
+      </BrowserRouter>
+    );
   }
-  };
-
+};
 
 
 ReactDOM.render(<App />, document.getElementById("App"));
