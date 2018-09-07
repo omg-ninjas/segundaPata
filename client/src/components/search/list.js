63a68e46da5f2c738bcc28cc3431f5eb729e8dcc
jazzcelaya <jazzcelaya@gmail.com>
2018-09-07 18:12:13 -0500

    reached database

--- client/src/components/search/list.js
+++ client/src/components/search/list.js
@@ -1,4 +1,5 @@
 import React, {Component}from "react";
+import Search from "./../../../../server/routes.js";
 
 
 class List extends React.Component {
@@ -13,6 +14,7 @@ class List extends React.Component {
         }
         handleSubmit(event) {
             alert('A name was submitted: ' + this.state.value);
+            // call search function & render
             event.preventDefault();
         }
         render() {
