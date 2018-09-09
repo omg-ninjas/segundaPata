c2b23149db790b4ea182ff4f00c2b640fec3c024
Henry2110 <henryvalladores@gmail.com>
2018-09-08 22:32:15 -0500

    header1

--- client/src/components/item-page/navigation.js
+++ client/src/components/item-page/navigation.js
@@ -3,7 +3,7 @@ import { NavLink } from 'react-router-dom';
 
 const Navigation = () => {
   return (
-    <div class="headerStyle">
+    <div className="headerStyle">
           <ul>
           <li><NavLink to="/">Home</NavLink></li>
           <li><NavLink to="/item">Item</NavLink></li>
