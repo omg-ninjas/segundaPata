87652af46b079f4e792b0fe1d8d5b3185ec4bb05
arnoldguadarrama41787 <arnoldguadarrama41787@gmail.com>
2018-09-12 10:29:58 -0500

    merging

--- server/schema.sql
+++ server/schema.sql
@@ -20,8 +20,8 @@ CREATE TABLE items (
  *  to create the database and the tables.*/
 
 INSERT INTO items (name, description, price, category, subcategory, comments) VALUES ("hueso mordido", "tamaño 8 pulgadas", "$10.00", "jugete", "para dientes", "todavia le queda %40");
-INSERT INTO items (name, description, price, category, subcategory, comments) VALUES ("camita", "tamaño 20 pulgadas", "$30.00", "cama", "color cafe", "tengo un cuadro como cama");
-INSERT INTO items (name, description, price, category, subcategory, comments) VALUES ("calsetas", "tamaño grande", "$10.00", "ropa", "abrigar", "son color moradas");
-INSERT INTO items (name, description, price, category, subcategory, comments) VALUES ("comida pedegri", "bolsa tamaño 400 oz", "$50.00", "comida", "dieta especial", "esta nueva la bolsa");
+INSERT INTO items (name, description, price, category, subcategory, comments) VALUES ("camita", "tamaño 20 pulgadas", "$30.00", "jugete", "color cafe", "tengo un cuadro como cama");
+INSERT INTO items (name, description, price, category, subcategory, comments) VALUES ("calsetas", "tamaño grande", "$10.00", "jugete", "abrigar", "son color moradas");
+INSERT INTO items (name, description, price, category, subcategory, comments) VALUES ("comida pedegri", "bolsa tamaño 400 oz", "$50.00", "jugete", "dieta especial", "esta nueva la bolsa");
 INSERT INTO items (name, description, price, category, subcategory, comments) VALUES ("zapato viejo", "nike", "$10.00", "jugete", "para dientes", "es un zapato negro con goma al cien");
-INSERT INTO items (name, description, price, category, subcategory, comments) VALUES ("sueter", "tamaño Chihuahua", "$25.00", "ropa", "color negro", "tenemos dos disponible");
+INSERT INTO items (name, description, price, category, subcategory, comments) VALUES ("sueter", "tamaño Chihuahua", "$25.00", "jugete", "color negro", "tenemos dos disponible");
