95fc8bc5ceba7c0b164be0f6a9640ee0f9513437
adrianjcorrea <adrianjcorrea007@gmail.com>
2018-09-18 18:50:30 -0500

    testing

--- client/dist/SearchBar.js
+++ client/dist/SearchBar.js
@@ -10,7 +10,7 @@ class SearchBar extends React.Component {
             // this.handleChange = this.handleChange.bind(this);
             // this.handleSubmit = this.handleSubmit.bind(this);
         }
-        handleChange(event) {
+        handleChange(event){
             this.setState({value: event.target.value});
         }
         // handleSubmit(event) {
