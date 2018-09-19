75fae434364ce04e15f16a7ef33886d78394847a
SthefaniaRodriguezL <stherod13@gmail.com>
2018-09-19 17:28:51 -0500

    added the icons for our footer

--- client/src/components/item-page/footer.js
+++ client/src/components/item-page/footer.js
@@ -1,13 +1,59 @@
-import React, { Component } from 'react';
 
-const Footer = () => (
+import React from "react";
+import { Col, Container, Row, Footer } from "mdbreact";
+import { SocialIcon } from "react-social-icons";
+// import "./Footer.css";
 
- <div className="col-md-12 down footers">
-   <footer>
-<h3>Creado por Mambo num 5.</h3>
-     </footer>
- </div>
-
-);
-
-export default Footer;
+class FooterPage extends React.Component {
+  render() {
+    return (
+      <Footer
+        color="unique-color-dark"
+        className="page-footer font-small pt-0"
+      >
+        <div style={{ backgroundColor: "#2892D7" }}>
+          <Container >
+            <Row className="py-4 d-flex align-items-center">
+              <Col
+                md="6"
+                lg="5"
+                className="text-center text-md-left mb-4 mb-md-0"
+              >
+                <h4>
+                  Get connected with us on social networks!
+                </h4>
+              </Col>
+              <Col
+                md="6"
+                lg="7"
+                className="text-center text-md-right"
+              >
+                <SocialIcon
+                  network="twitter"
+                  color="#00aced"
+                  url="https://twitter.com/petcomexico?lang=en"
+                />
+                <SocialIcon
+                  network="facebook"
+                  color="#3b5998"
+                  url="https://www.facebook.com/Petco/"
+                />
+                <SocialIcon
+                  network="instagram"
+                  color="#bc2a8d"
+                  url="https://www.instagram.com/petcomexico/"
+                />
+              </Col>
+            </Row>
+          </Container>
+        </div>
+        <div className="footer-copyright text-center py-3">
+          <Container fluid>
+            &copy; {new Date().getFullYear()} Copyright:{" OMG NINJAS "}
+          </Container>
+        </div>
+      </Footer>
+    );
+  }
+}
+export default FooterPage;
