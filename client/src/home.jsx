9a9fc60af12549d8321daf5d43d17ae3c8fd96d0
SthefaniaRodriguezL <stherod13@gmail.com>
2018-09-19 09:36:20 -0500

    changes for like and share button

--- client/src/home.jsx
+++ client/src/home.jsx
@@ -10,71 +10,71 @@ import UploadProduct from "./components/UploadProduct.jsx";
 
 
 class Home extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      items: [],
-      products: undefined,
-      product: undefined
-    }
-    this.setProduct = this.setProduct.bind(this);
-    this.handleSubmitToys = this.handleSubmitToys.bind(this);
-    this.handleSubmitClothes=this.handleSubmitClothes.bind(this);
-    this.handleSubmitBeds=this.handleSubmitBeds.bind(this);
-    this.handleSubmitAccesories= this.handleSubmitAccesories.bind(this);
-  }
-
-  componentDidMount() {
-    axios.get('/items')
-    .then(res => {
-      const items = res.data;
-      this.setState({ items });
-    })
-  }
-
- handleSubmitToys(){
-   //alert('toys called');
-  axios.get('/toys')
-  .then(res => {
-    console.log(res);
-   var products = res.data;
-    this.setState({products});
-  })
+ constructor(props) {
+   super(props);
+   this.state = {
+     items: [],
+     products: undefined,
+     product: undefined
+   }
+   this.setProduct = this.setProduct.bind(this);
+   this.handleSubmitToys = this.handleSubmitToys.bind(this);
+   this.handleSubmitClothes=this.handleSubmitClothes.bind(this);
+   this.handleSubmitBeds=this.handleSubmitBeds.bind(this);
+   this.handleSubmitAccesories= this.handleSubmitAccesories.bind(this);
  }
 
- handleSubmitClothes(){
-   //alert('toys called');
-  axios.get('/clothes')
-  .then(res => {
-    console.log(res);
-   var products = res.data;
-    this.setState({products});
-  })
- }
- handleSubmitBeds(){
-   //alert('toys called');
-  axios.get('/beds')
-  .then(res => {
-    console.log(res);
-   var products = res.data;
-    this.setState({products});
-  })
+ componentDidMount() {
+   axios.get('/items')
+   .then(res => {
+     const items = res.data;
+     this.setState({ items });
+   })
  }
 
- handleSubmitAccesories(){
-   //alert('toys called');
-  axios.get('/accesories')
-  .then(res => {
-    console.log(res);
-   var products = res.data;
-    this.setState({products});
-  })
- }
+handleSubmitToys(){
+  //alert('toys called');
+ axios.get('/toys')
+ .then(res => {
+   console.log(res);
+  var products = res.data;
+   this.setState({products});
+ })
+}
+
+handleSubmitClothes(){
+  //alert('toys called');
+ axios.get('/clothes')
+ .then(res => {
+   console.log(res);
+  var products = res.data;
+   this.setState({products});
+ })
+}
+handleSubmitBeds(){
+  //alert('toys called');
+ axios.get('/beds')
+ .then(res => {
+   console.log(res);
+  var products = res.data;
+   this.setState({products});
+ })
+}
+
+handleSubmitAccesories(){
+  //alert('toys called');
+ axios.get('/accesories')
+ .then(res => {
+   console.log(res);
+  var products = res.data;
+   this.setState({products});
+ })
+}
 
 setProduct(id){
-  this.setState({
-    product: id
-  })
+ this.setState({
+   product: id
+ })
 }
 
 // postItem(Upload){
@@ -93,26 +93,26 @@ setProduct(id){
 
 
 render(){
-  return(
-    <div>
-      <center>
-        <h1>
-          <img src="https://i.imgur.com/hxDOW9A.jpg" title="Una pagina para tus mascotas!" />
-          <div>Segunda Pata</div>
-        </h1>
-        <h2>Vende y Compra Accesorios, para Mascotas.</h2>
-        <SearchBar items={this.state.items} handleSubmitToys={this.handleSubmitToys}
-        handleSubmitClothes={this.handleSubmitClothes} handleSubmitBeds={this.handleSubmitBeds}
-        handleSubmitAccesories={this.handleSubmitAccesories}
-        />
-        <ItemsHome items={this.state.items} products={this.state.products} setProduct={this.setProduct}/>
-      </center>
-      <div className="fb-like" data-href="https://github.com/mambo-num-5" data-layout="standard" data-action="like" data-size="large" data-show-faces="true" data-share="true"></div>
-    </div>
-    )
-  }
+ return(
+   <div>
+     <center>
+       <h1>
+         <img src="https://i.imgur.com/hxDOW9A.jpg
+https://i.imgur.com/hxDOW9A.jpg
+" title="Una pagina para tus mascotas!" />
+         <div>Segunda Pata</div>
+       </h1>
+       <h2>Vende y Compra Accesorios, para Mascotas.</h2>
+       <SearchBar items={this.state.items} handleSubmitToys={this.handleSubmitToys}
+       handleSubmitClothes={this.handleSubmitClothes} handleSubmitBeds={this.handleSubmitBeds}
+       handleSubmitAccesories={this.handleSubmitAccesories}
+       />
+       <ItemsHome items={this.state.items} products={this.state.products} setProduct={this.setProduct}/>
+     </center>
+     <div className="fb-like" data-href="https://www.petco.com/shop/en/petcostore/category/dog" data-layout="standard" data-action="like" data-size="large" data-show-faces="true" data-share="true"></div>
+   </div>
+   )
+ }
 };
-
-
 
 export default Home;
--- server/index.js
+++ server/index.js
@@ -8,85 +8,85 @@ app.use(express.static(__dirname + '/../client/dist'));
 
 
 app.get('/items', function (req, res) {
-  items.selectAll(function(err, data) {
-    if(err) {
-      res.sendStatus(500);
-    } else {
-      console.log("get all items request performed")
-      res.json(data);
-    }
-  });
+ items.selectAll(function(err, data) {
+   if(err) {
+     res.sendStatus(500);
+   } else {
+     console.log("get all items request performed")
+     res.json(data);
+   }
+ });
 });
 app.get('/toys', function (req, res) {
-  items.selectToys(function(err, data) {
-    if(err) {
-      console.log(err);
-      res.sendStatus(500);
-    } else {
-      console.log("get all toys request performed")
-      res.json(data);
-    }
-  });
+ items.selectToys(function(err, data) {
+   if(err) {
+     console.log(err);
+     res.sendStatus(500);
+   } else {
+     console.log("get all toys request performed")
+     res.json(data);
+   }
+ });
 });
 
 app.get('/clothes', function (req, res) {
-  items.selectClothes(function(err, data) {
-    if(err) {
-      console.log(err);
-      res.sendStatus(500);
-    } else {
-      res.json(data);
-    }
-  });
+ items.selectClothes(function(err, data) {
+   if(err) {
+     console.log(err);
+     res.sendStatus(500);
+   } else {
+     res.json(data);
+   }
+ });
 });
 
 app.get('/beds', function (req, res) {
-  items.selectBeds(function(err, data) {
-    if(err) {
-      console.log(err);
-      res.sendStatus(500);
-    } else {
-      res.json(data);
-    }
-  });
+ items.selectBeds(function(err, data) {
+   if(err) {
+     console.log(err);
+     res.sendStatus(500);
+   } else {
+     res.json(data);
+   }
+ });
 });
 
 app.get('/accesories', function (req, res) {
-  items.selectAccesories(function(err, data) {
-    if(err) {
-      console.log(err);
-      res.sendStatus(500);
-    } else {
-      console.log("get all accesories request performed")
-      res.json(data);
-    }
-  });
+ items.selectAccesories(function(err, data) {
+   if(err) {
+     console.log(err);
+     res.sendStatus(500);
+   } else {
+     console.log("get all accesories request performed")
+     res.json(data);
+   }
+ });
 });
 
 
 app.post('/items', function(req, res) {
-  let name = req.body.name;
-  let descrip = req.body.descrip;
-  let price = req.body.price;
-  let category = req.body.category;
-  let email = req.body.email;
-  let vendor = req.body.vendor;
-  console.log(name);
-  if (!name) {
-    res.sendStatus(400);
-  } else {
-    items.insertProduct(name, descrip, price, category, email, vendor, (err, results) => {
-      if (err) {
-        res.sendStatus(500);
-      } else {
-        res.status(200).json(results);
-      }
-    });
-  }
+ let name = req.body.name;
+ let descrip = req.body.descrip;
+ let price = req.body.price;
+ let category = req.body.category;
+ let email = req.body.email;
+ let vendor = req.body.vendor;
+ console.log(name);
+ if (!name) {
+   res.sendStatus(400);
+ } else {
+   items.insertProduct(name, descrip, price, category, email, vendor, (err, results) => {
+     if (err) {
+       res.sendStatus(500);
+     } else {
+       res.status(200).json(results);
+     }
+   });
+ }
 });
 
 
 
 app.listen(3000, function() {
-  console.log('listening on port 3000!');
+ console.log('listening on port 3000!');
 });
