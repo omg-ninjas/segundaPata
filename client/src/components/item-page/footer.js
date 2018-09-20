1d80480dce42584720aaa39e0571b195f9ad3705
SthefaniaRodriguezL <stherod13@gmail.com>
2018-09-20 11:55:10 -0500

    removed links and added a button

--- client/src/components/item-page/footer.js
+++ client/src/components/item-page/footer.js
@@ -20,7 +20,7 @@ class FooterPage extends React.Component {
                 className="text-center text-md-left mb-4 mb-md-0"
               >
                 <h4>
-                  Get connected with us on social networks!
+                  Information about stores!
                 </h4>
               </Col>
               <Col
---
+++ client/src/components/item-page/shareButton.js
@@ -0,0 +1,9 @@
+// import React { Component } from 'react';
+
+// class shareButton extends Component {
+
+// <img src="https://beginnersbook.com/wp-content/uploads/2013/09/fblike1.jpg"></img>
+
+   // }
+// 
+// export default shareButton;
