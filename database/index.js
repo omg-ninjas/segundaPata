67d9a5159aa05b2af4bf07980c4d90e7795d1086
fatimaavilesfa <fatimaaviles1612@gmail.com>
2018-09-22 09:32:59 -0500

    change password in mysql

--- database/index.js
+++ database/index.js
@@ -3,7 +3,7 @@ var mysql = require('mysql');
 var connection = mysql.createConnection({
   host     : 'localhost',
   user     : 'root',
-  password : 'holacode',
+  password : 'almendra.4',
   database : 'segundaPata'
 });
 
