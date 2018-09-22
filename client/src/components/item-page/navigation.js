76c5abcfe05ae5a3c4647d2bf79ef57555ea988a
fatimaavilesfa <fatimaaviles1612@gmail.com>
2018-09-22 11:54:40 -0500

    spelling fixed

--- client/src/components/item-page/navigation.js
+++ client/src/components/item-page/navigation.js
@@ -8,7 +8,7 @@ const Navigation = () => {
     <nav className="headerStyle">
           <ul>
           <NavLink to="/">Casa</NavLink>
-          <NavLink to="/Upload">Subir articulo</NavLink>
+          <NavLink to="/Upload">subir</NavLink>
           <NavLink to="/Info">Subir articulo</NavLink>
 
           </ul>
