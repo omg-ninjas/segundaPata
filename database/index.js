5810e5cc74aa9f4144556de0b1f698c6ac862c72
adrianjcorrea <40035105+adrianjcorrea@users.noreply.github.com>
2018-09-24 15:06:39 -0500

    changed changes

--- database/index.js
+++ database/index.js
@@ -3,7 +3,7 @@ var mysql = require('mysql');
 var connection = mysql.createConnection({
   host     : 'localhost',
   user     : 'root',
-  password : 'almendra.4',
+  password : 'holacode',
   database : 'segundaPata'
 });
 
